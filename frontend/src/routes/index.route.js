1edc83f802e1628c6a25f9cadd8672979f8f4221
ducewcgke2005 <ducvjpro04@gmail.com>
2026-01-30 15:45:23 +0700

    inventory route

--- frontend/src/routes/index.route.js
+++ frontend/src/routes/index.route.js
@@ -1,10 +1,12 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import salesRoute from "./sales.route";
+import InventoryRoutes from "./inventory.route";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {salesRoute}
+      <Route path="/inventory/*" element={<InventoryRoutes />} />
     </Routes>
   );
 };
