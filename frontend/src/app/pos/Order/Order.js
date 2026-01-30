846af7bcaafcd641d2b45ef7ccbea96cadbec0e7
hoangnguyen175 <hoangzk255@gmail.com>
2026-01-30 15:41:25 +0700

    feat: create component Order

---
+++ frontend/src/app/pos/Order/Order.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import OrderItemList from "./OrderItemList/OrderItemList";
+
+export default function SalesHome() {
+  return (
+    <div>
+      <OrderItemList />
+    </div>
+  );
+}
