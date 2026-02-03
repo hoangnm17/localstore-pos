9abd68c7825e3f3d2d20bd430417084daf9bd70d
hoangnguyen175 <hoangzk255@gmail.com>
2026-02-04 00:28:59 +0700

    feat: create payment modal to post api

--- frontend/src/app/pos/Order/Order.js
+++ frontend/src/app/pos/Order/Order.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { invoiceCreate } from "../../../services/Invoices/invoice.service";
 import OrderItemList from "./OrderItemList/OrderItemList";
 import PaymentDetail from "./Payment/PaymentDetail";
 import CustomerSearch from "./Customer/CustomerSearch";
+import PaymentModal from "./Payment/PaymentModal";
 
 export default function Order({ orderItems, increase, decrease, remove }) {
+  const [showPayment, setShowPayment] = useState(false);
+  const [paymentContext, setPaymentContext] = useState(null);
+
+  const handleOpenPayment = (data) => {
+    setPaymentContext(data);
+    setShowPayment(true);
+  };
+
+  const handleConfirmPayment = (paymentInfo) => {
+    const payload = {
+      items: paymentContext.items,
+      customer: paymentContext.customer,
+      total: paymentContext.total,
+      payment: paymentInfo,
+    };
+
+    console.log("POST ORDER:", payload);
+    invoiceCreate(payload)
+    setShowPayment(false);
+  };
 
   const isEmpty = orderItems.length === 0;
 
@@ -41,8 +64,18 @@ export default function Order({ orderItems, increase, decrease, remove }) {
           boxShadow: "0 -2px 8px rgba(0,0,0,0.05)"
         }}
       >
-        <PaymentDetail items={orderItems} />
+        <PaymentDetail
+          items={orderItems}
+          onOpenPayment={handleOpenPayment} />
       </div>
+
+      {showPayment && (
+        <PaymentModal
+          total={paymentContext.total}
+          onClose={() => setShowPayment(false)}
+          onConfirm={handleConfirmPayment}
+        />
+      )}
 
     </div>
   );
--- frontend/src/app/pos/Order/Payment/PaymentDetail.js
+++ frontend/src/app/pos/Order/Payment/PaymentDetail.js
@@ -1,21 +1,28 @@
-const PaymentDetail = ({ items, customer, onPay }) => {
-    const total = items?.reduce(
-        (sum, i) => sum + i.unitPrice * i.quantity,
-        0
-    )
+const PaymentDetail = ({ items, customer, onOpenPayment }) => {
+  const total = items.reduce(
+    (sum, i) => sum + i.unitPrice * i.quantity,
+    0
+  );
 
-    return (
-        <div className="p-4 bg-white border-t">
-            <h4>Tổng thanh toán: {total.toLocaleString()} đ</h4>
-            <button
-                disabled={items.length === 0}
-                onClick={() => onPay({ items, customer, total })}
-                className="w-full bg-blue-600 text-black py-3 rounded"
-            >
-                Thanh toán
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="p-4 bg-white border-t">
+      <h4>Tổng thanh toán: {total.toLocaleString()} đ</h4>
 
-export default PaymentDetail
+      <button
+        disabled={items.length === 0}
+        onClick={() =>
+          onOpenPayment({
+            items,
+            customer,
+            total,
+          })
+        }
+        className="w-full bg-blue-600 text-white py-3 rounded"
+      >
+        Thanh toán
+      </button>
+    </div>
+  );
+};
+
+export default PaymentDetail;
