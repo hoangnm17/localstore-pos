d4468165cf8d394b73b5ee67b1ac1b8d52546cba
ttrangdt <ttrang.wk@gmail.com>
2026-02-04 13:38:53 +0700

    frontend: Page Login UI

--- frontend/src/routes/index.route.js
+++ frontend/src/routes/index.route.js
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import salesRoute from "./sales.route";
 import InventoryRoutes from "./inventory.route";
-
+import authRoute from "./auth.route";
 const AppRoutes = () => {
   return (
     <Routes>
+      {authRoute}
       {salesRoute}
       {InventoryRoutes}
       {/* <Route path="/inventory/*" element={<InventoryRoutes />} /> */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 };
