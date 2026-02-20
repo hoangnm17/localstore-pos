dc132cad92eaf9203d8b225d4cc499123f0d5e73
ducewcgke2005 <ducvjpro04@gmail.com>
2026-02-20 13:38:06 +0700

    frontend: fetch api problematic reports

--- frontend/src/routes/inventory.route.js
+++ frontend/src/routes/inventory.route.js
@@ -3,6 +3,7 @@ import CategoryStock from "../app/inventory/pages/CategoryStock";
 import ProductStock from "../app/inventory/pages/ProductStock";
 import MainLayout from "../layouts/MainLayout";
 import InventoryMenu from "../app/inventory/pages/InventoryMenu";
+import ProblematicReport from "../app/inventory/pages/ProblematicReport";
 
 const InventoryRoutes = (
     <Route path="/inventory" element={<MainLayout />}>
@@ -11,6 +12,7 @@ const InventoryRoutes = (
             path="categories/:categoryId/products"
             element={<ProductStock />}
         />
+        <Route path="problematic" element={<ProblematicReport />} />
         <Route path="menu" element={<InventoryMenu />} />
     </Route>
 );
