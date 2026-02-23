0af8ddf6a244b969cc348b97b6f649fac9c4b179
hoangnguyen175 <hoangzk255@gmail.com>
2026-02-24 00:21:00 +0700

    refactor: use jsconfig

--- frontend/src/app/pos/Order/Payment/PaymentDetail.js
+++ frontend/src/app/pos/Order/Payment/PaymentDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatCurrency } from "../../../../utils/formatters";
+import { formatCurrency } from "utils/formatters";
 
 const PaymentDetail = ({
   total = 0,
