4b88568bf8fb362f1678f827fc184217a6adb43a
ttrangdt <ttrang.wk@gmail.com>
2026-02-26 03:53:39 +0700

    fix: remove protectedRoute

--- frontend/src/routes/index.route.js
+++ frontend/src/routes/index.route.js
@@ -2,37 +2,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import salesRoute from "./sales.route";
 import InventoryRoutes from "./inventory.route";
 import authRoute from "./auth.route";
-import ProtectedRoute from "./protected.route";
+// import CategoryRoutes from "./category.route.js";
 import staffRoute from "./staff.route";
-import crmRoute from "./crm.route";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {authRoute}
-      {/* <Route element={<ProtectedRoute allowedRoles={['Manager', 'Cashier']} />}>
-        {salesRoute}
-      </Route>
-      <Route element={<ProtectedRoute allowedRoles={['Manager', 'Warehouse']} />}>
-        {InventoryRoutes}
-      </Route> */}
-    <Route element={<ProtectedRoute requiredFeatures={["VIEW_STAFF"]}/>}>
-        {staffRoute}
-      </Route>
-      <Route element={<ProtectedRoute requiredFeature="VIEW_SALES" />}>
-        {salesRoute}
-      </Route>
-
-      <Route element={<ProtectedRoute requiredFeature="VIEW_INVENTORY" />}>
-        {InventoryRoutes}
-      </Route>
-
-      {crmRoute}
-
+      {salesRoute}
+      {InventoryRoutes}
+      {staffRoute}
+      {/* {CategoryRoutes} */}
       {/* <Route path="/inventory/*" element={<InventoryRoutes />} /> */}
       <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/inventory/*" element={<InventoryRoutes />} />
+      {/* <Route path="/categories/*" element={<CategoryRoutes />} /> */}
     </Routes>
   );
 };
-
 export default AppRoutes;
