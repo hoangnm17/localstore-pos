05ef125d285a705bb006269cf890437e41a568ec
ttrangdt <ttrang.wk@gmail.com>
2026-02-25 19:53:50 +0700

    feat: Add logout and clear auth

--- frontend/src/services/Auth/auth.service.js
+++ frontend/src/services/Auth/auth.service.js
@@ -3,3 +3,7 @@ import api from "../axiosInstance";
 export const loginAPI = (data) => {
   return api.post("/auth/login", data);
 };
+export const logout = () => {
+  localStorage.clear();
+  window.location.href = "/login";
+};
