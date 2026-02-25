6be7bbc27848c63dc7ee0ddeebe018e050858e5f
ttrangdt <ttrang.wk@gmail.com>
2026-02-25 15:04:22 +0700

    feat: add staffdetail feature

--- frontend/src/routes/staff.route.js
+++ frontend/src/routes/staff.route.js
@@ -2,12 +2,14 @@ import { Route } from "react-router-dom";
 import StaffList from "../app/staff/StaffList"; 
 import StaffCreate from "../app/staff/StaffCreate";
 import StaffUpdate from "../app/staff/StaffUpdate";
+import StaffDetail from "../app/staff/StaffDetail";
 
 const staffRoute = (
   <>
     <Route path="/staff" element={<StaffList />} />
     <Route path="/staff/create" element={<StaffCreate />} />
     <Route path="/staff/update" element={<StaffUpdate />} />
+    <Route path="/staff/detail" element={<StaffDetail />} />
   </>
 );
 
