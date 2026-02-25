9430791a8055533f9c05ba8ee550c8a0df6a74b5
ttrangdt <ttrang.wk@gmail.com>
2026-02-25 19:54:47 +0700

    feat: Add POST /logout route to auth router

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
-
+router.post('/logout', authController.logout);
 module.exports = router;
