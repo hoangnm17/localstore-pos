bb73b446d9af8e547496dd4370f62e961dd57269
ThuNTM <thuntmhe180810@fpt.edu.vn>
2026-02-26 14:51:26 +0700

    fix path of product

--- frontend/src/routes/product.route.js
+++ frontend/src/routes/product.route.js
@@ -1,6 +1,6 @@
 import { Route } from 'react-router-dom';
 import ManagerLayout from '../layouts/ManagerLayout';
-import ProductList from '../app/product/ProductList';
+import ProductList from '../app/product/productList';
 
 const ProductRoutes = (
     <Route
