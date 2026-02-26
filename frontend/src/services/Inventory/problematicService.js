639fa9ae6c0e00a4490e83f350f9dfbb17025185
ducewcgke2005 <ducvjpro04@gmail.com>
2026-02-26 21:25:30 +0700

    backend update api problematic report

---
+++ frontend/src/services/Inventory/problematicService.js
@@ -0,0 +1,23 @@
+import api from "../axiosInstance";
+
+function getReports(filters = {}) {
+  return api.get("/inventory/reports/list", {
+    params: filters
+  });
+}
+
+function createReport(data) {
+  return api.post("/inventory/reports/send", data);
+}
+
+function updateReportStatus(reportId, status) {
+  return api.put(`/inventory/reports/${reportId}/status`, {
+    status
+  });
+}
+
+export default {
+  getReports,
+  createReport,
+  updateReportStatus
+};
