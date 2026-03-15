e9f7ed8c6cb9cfa7fe4a147cf674640665c27b97
hoangnguyen175 <hoangzk255@gmail.com>
2026-03-15 22:54:57 +0700

    feat: create return api

---
+++ backend/src/controllers/return.controller.js
@@ -0,0 +1,121 @@
+const returnService = require("../services/return.service")
+
+
+const getReturns = async (req, res) => {
+  try {
+
+    const { status, page, pageSize } = req.query;
+
+    const data = await returnService.getReturns({
+      status,
+      page,
+      pageSize
+    });
+
+    res.json({
+      success: true,
+      ...data
+    });
+
+  } catch (err) {
+
+    res.status(400).json({
+      success: false,
+      message: err.message
+    });
+
+  }
+};
+
+const getReturnDetail = async (req, res) => {
+  try {
+
+    const data = await returnService.getReturnDetail(req.params.id);
+
+    res.json({
+      success: true,
+      data
+    });
+
+  } catch (err) {
+
+    res.status(400).json({
+      success: false,
+      message: err.message
+    });
+
+  }
+};
+
+const createReturn = async (req, res) => {
+    
+    
+    try {
+        const data = await returnService.createReturn(req.user, req.body);
+
+        res.json({
+            success: true,
+            data
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+const approveReturn = async (req, res) => {
+    try {
+        const data = await returnService.approveReturn(req.user, req.params.id);
+        res.json({ success: true, data });
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
+
+const rejectReturn = async (req, res) => {
+    try {
+
+        const data = await returnService.rejectReturn(
+            req.user,
+            req.params.id,
+            req.body.reason
+        );
+
+        res.json({
+            success: true,
+            data
+        });
+
+    } catch (err) {
+
+        res.status(400).json({
+            success: false,
+            message: err.message
+        });
+
+    }
+};
+
+const restockReturn = async (req, res) => {
+    try {
+        const data = await returnService.restockReturn(
+            req.user,
+            req.params.id,
+            req.body.items || []
+        );
+        res.json({ success: true, data });
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message });
+    }
+}
+
+module.exports = {
+    getReturns,
+    getReturnDetail,
+    createReturn,
+    approveReturn,
+    restockReturn,
+    rejectReturn,
+}
