fa0938dd5394238d5a37e86b65cbddadee68d1fc
hoangnguyen175 <hoangzk255@gmail.com>
2026-03-16 23:41:32 +0700

    refactor: change layout

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 function MainLayout() {
     return (
         <>
             <Header />
-            <main className="min-vh-100">
-                <Outlet />
-            </main>
+            <div style={{ display: 'flex' }}>
+                <Sidebar />
+                <main className="min-vh-100" style={{ flex: 1, overflow: 'auto' }}>
+                    <Outlet />
+                </main>
+            </div>
             <Footer />
         </>
     );
