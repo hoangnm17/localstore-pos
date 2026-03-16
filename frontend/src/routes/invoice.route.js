43b7bf85c77b63c273df880f2765abbb47b37d11
hoangnguyen175 <hoangzk255@gmail.com>
2026-03-16 23:57:20 +0700

    refactor: change layout route

--- frontend/src/routes/invoice.route.js
+++ frontend/src/routes/invoice.route.js
@@ -1,10 +1,8 @@
 import { Route } from "react-router-dom";
 import InvoicePage from "app/invoice/InvoicePage";
-import MainLayout from "layouts/MainLayout";
 
 const InvoiceRoutes = (
-  <Route element={<MainLayout />}>
-  <Route path="/invoices" element={<InvoicePage />} />
+  <Route path="/invoices" element={<InvoicePage />}>
   </Route>
 );
 
