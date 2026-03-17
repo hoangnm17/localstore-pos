1abda89e6dfeef431617de8336f3ab43fd32c5cf
ducewcgke2005 <ducvjpro04@gmail.com>
2026-03-17 23:54:01 +0700

    update api fit with database updated

--- backend/src/controllers/invoice.controller.js
+++ backend/src/controllers/invoice.controller.js
@@ -68,34 +68,22 @@ const getAllInvoice = async (req, res) => {
 
 const createInvoice = async (req, res) => {
   try {
-    const staffId = req.user?.id;
-    const counterId = req.user?.counterId;
+    const userId = req.user?.id;
+    if (!userId) 
+      return res.status(401).json({ 
+    success: false, 
+    message: "Unauthorized" 
+  });
 
-    if (!staffId) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized",
-      });
-    }
-
-    if (!counterId) {
-      return res.status(500).json({
-        success: false,
-        message: "Missing Counter",
-      })
-    }
+    const { staffId } = await invoiceService.syncCounter(userId, COUNTER_ID);
 
     const result = await invoiceService.createInvoice({
       ...req.body,
       staffId,
-      counterId,
+      counterId: COUNTER_ID,
     });
 
-    return res.status(201).json({
-      success: true,
-      data: result,
-    });
-
+    return res.status(201).json({ success: true, data: result });
   } catch (err) {
     return handleError(res, err);
   }
