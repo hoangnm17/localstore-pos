31973a5e1cee06c57c7e7f5dc364f598c1c7a51d
ttrangdt <ttrang.wk@gmail.com>
2026-03-24 20:47:17 +0700

    feat: add  change password modal

--- frontend/src/layouts/AppLayout.js
+++ frontend/src/layouts/AppLayout.js
@@ -1,23 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Header from '../components/Header/Header';
+import ForceChangePasswordModal from '../app/auth/modals/ForceChangePasswordModal';
 
-/**
- * AppLayout — Layout chính dùng <Outlet> của React Router v6.
- * Bao gồm: Sidebar (trái) + Header (trên) + Footer (dưới)
- * Chỉ cần bọc nhóm route trong <Route element={<AppLayout />}>
- * thì tất cả page con tự có Sidebar + Header mà KHÔNG cần import trong từng page.
- */
 function AppLayout() {
+    let requireChange = false;
+    try {
+        const userStr = localStorage.getItem('user');
+        if (userStr) {
+            const userObj = JSON.parse(userStr);
+            if (userObj.requirePasswordChange === true) {
+                requireChange = true;
+            }
+        }
+    } catch (e) {
+        console.error("Lỗi Parsing User:", e);
+    }
     return (
-        <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+        <div style={{ display: 'flex', height: '100vh', overflow: 'hidden', position: 'relative' }}>
+            
+            {requireChange && <ForceChangePasswordModal />}
             <Sidebar />
             <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
                 <Header />
                 <main style={{ flex: 1, overflowY: 'auto', background: '#f0f2f5' }}>
                     <Outlet />
                 </main>
-                {/* ✅ Footer gọn cho admin dashboard */}
                 <footer style={{
                     height: '36px',
                     background: '#fff',
